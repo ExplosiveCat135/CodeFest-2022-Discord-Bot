9334f0d33d1a199807aa39d206b2ce12fdef51e3
Harry Tait <98203501+smallfun1@users.noreply.github.com>
2022-01-22 15:05:07 +0000

    added more facts

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import os
 
 Websites = ["https://www.energy.gov/ne/articles/advantages-and-challenges-nuclear-energy#:~:text=Nuclear%20energy%20protects%20air%20quality,medical%20field%20to%20space%20exploration.","https://www.eia.gov/energyexplained/nuclear/","https://www.eia.gov/energyexplained/nuclear/nuclear-power-plants.php","https://energyeducation.ca/encyclopedia/Nuclear_power_plant","https://en.wikipedia.org/wiki/Nuclear_power_plant#:~:text=A%20nuclear%20power%20plant%20(sometimes,a%20generator%20that%20produces%20electricity.","https://www.encyclopedie-energie.org/en/nuclear-energy-brief-history/"]
 
-my_secret = os.environ['FishySecret']
+Yes = os.environ['FishySecret']
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
 
-  print("Le educational bot is online and being fishy (as always)")
+  print("The educational bot is online and being fishy (as always)")
 
   print("---------------------------------------------")
-  print("The prefix is &\n\n&website: to show websites about nuclear energy\n&history: to show glimpses of history about nuclear power plants.")
+  print("The prefix is &\n\n&website: to show websites about nuclear energy\n&history: to show glimpses of history about nuclear power plants.\n&fact: to show some random facts about nuclear energy")
   #Prefixes Commands ^^^
   print("---------------------------------------------")
 
-def readQuotes(fileName):
+def Readfile(fileName):
   myFile = open(fileName,"r")
   Very_Fishy_Quotes = []
   for line in myFile:
@@ -30,22 +30,20 @@ def readQuotes(fileName):
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  if message.content.startswith("&website"):
-    
-    await message.channel.send(Websites[random.randint(0,len(Websites-1))
+ 
 
   if message.content.startswith("&history"):
-    my_fishy = readQuotes("history_about_nuclear_energy.txt") 
+    my_fishy = Readfile("history_about_nuclear_energy.txt") 
     await message.channel.send(random.choice(my_fishy))
-
   
+  if message.content.startswith("&website"):
+    await message.channel.send(Websites[random.randint(0,len(Websites)-1)])
+
+  if message.content.startswith("&fact"):
+    facts = Readfile("Facts about nuclear power.txt")
+    await message.channel.send(random.choice(facts))
+
     
+client.run(Yes)
 
-
-
-
-client.run(my_secret)
 
