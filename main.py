aa8d2f4063ead7d3a574b52f2f432d5b90b66644
ExplosiveCat131 <92578989+ExplosiveCat135@users.noreply.github.com>
2022-01-22 11:59:36 +0000

    something I don't know about

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import random
 import os 
 
-Websites = ["https://www.energy.gov/ne/articles/advantages-and-challenges-nuclear-energy#:~:text=Nuclear%20energy%20protects%20air%20quality,medical%20field%20to%20space%20exploration.","https://www.eia.gov/energyexplained/nuclear/","https://www.eia.gov/energyexplained/nuclear/nuclear-power-plants.php",]
+Websites = ["https://www.energy.gov/ne/articles/advantages-and-challenges-nuclear-energy#:~:text=Nuclear%20energy%20protects%20air%20quality,medical%20field%20to%20space%20exploration.","https://www.eia.gov/energyexplained/nuclear/","https://www.eia.gov/energyexplained/nuclear/nuclear-power-plants.php","https://energyeducation.ca/encyclopedia/Nuclear_power_plant","https://en.wikipedia.org/wiki/Nuclear_power_plant#:~:text=A%20nuclear%20power%20plant%20(sometimes,a%20generator%20that%20produces%20electricity."]
 
 my_secret = os.environ['FishySecret']
 
@@ -14,7 +14,7 @@ async def on_ready():
   print("Le educational bot is online and being fishy (as always)")
 
   print("---------------------------------------------")
-  print("The prefix is &\n\n&source: to show creditable websites of nuclear energy")
+  print("The prefix is &\n\n&webiste: to show websites about nuclear energy\n&history: to show glimphs of history about nuclear power plants.")
   #Prefixes Commands ^^^
   print("---------------------------------------------")
 
@@ -22,13 +22,15 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+  
+  if message.content.startswith("&website"):
+    
+    await message.channel.send(Websites[random.randint(0,4)])
 
-  if message.content.startswith("prefix"):
-    cycle = 0
-    await message.channel.send(Websites[cycle])
-    cycle = cycle + 1
-    if cycle == 2:
-      cycle = 1
+  if message.content.startswith("")
+
+  
+    
 
 
 
