eb62655e94e28b21d993f563e6f110c62ad82041
ExplosiveCat131 <92578989+ExplosiveCat135@users.noreply.github.com>
2022-01-23 03:17:34 +0000

    added command &main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import discord
 import random
 import os 
+from dotenv import load_dotenv
 
+#DO NOT SPAM IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 Websites = ["https://www.energy.gov/ne/articles/advantages-and-challenges-nuclear-energy#:~:text=Nuclear%20energy%20protects%20air%20quality,medical%20field%20to%20space%20exploration.","https://www.eia.gov/energyexplained/nuclear/","https://www.eia.gov/energyexplained/nuclear/nuclear-power-plants.php","https://energyeducation.ca/encyclopedia/Nuclear_power_plant","https://en.wikipedia.org/wiki/Nuclear_power_plant#:~:text=A%20nuclear%20power%20plant%20(sometimes,a%20generator%20that%20produces%20electricity.","https://www.encyclopedie-energie.org/en/nuclear-energy-brief-history/"]
 
 Yes = os.environ['FishySecret']
-
+Guild = "Faq about nuclear power (Codefest 2022)"
 client = discord.Client()
 
 @client.event
@@ -14,8 +16,8 @@ async def on_ready():
   print("The educational bot is online and being fishy (as always)")
 
   print("---------------------------------------------")
-  print("The prefix is &\n\n&website: to show websites about nuclear energy\n&history: to show glimpses of history about nuclear power plants.\n&fact: to show some random facts about nuclear energy")
-  #Prefixes Commands ^^^
+  print("The prefix is &\n\n&website: to show websites about nuclear energy\n&history: to show glimpses of history about nuclear power plants.\n&fact: to show some random facts about nuclear energy\n&main: to show the link to the main website")
+  #Prefixe Commands ^^^
   print("---------------------------------------------")
 
 def Readfile(fileName):
@@ -42,6 +44,9 @@ async def on_message(message):
   if message.content.startswith("&fact"):
     facts = Readfile("Facts about nuclear power.txt")
     await message.channel.send(random.choice(facts))
+
+  if message.content.startswith("&main"):
+    await message.channel.send("https://website-on-nuclear-power.harrytait.repl.co/index.html")
 
     
 client.run(Yes)
